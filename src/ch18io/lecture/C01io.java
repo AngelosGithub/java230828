bdf3fa796a3a443afca9534f46924d36f57643da
Angelo <ljy6037@gmail.com>
2023-09-14 17:33:29 +0900

    230914 ioStream

---
+++ src/ch18io/lecture/C01io.java
@@ -0,0 +1,11 @@
+package ch18io.lecture;
+
+public class C01io {
+    public static void main(String[] args) {
+        // io stream 4종류
+        // byte단위 입력 스트림 : input stream
+        // byte단위 출력 스트림 : output stream
+        // 문자단위 입력 스트림 : Reader
+        // 문자단위 출력 스트림 : Writer
+    }
+}
